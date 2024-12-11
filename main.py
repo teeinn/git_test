e9ed2e94a16dd669554612eaa13ac22ed39f51c3
teeinn <dainn1992@naver.com>
2024-12-12 00:33:01 +0900

    simple change

--- main.py
+++ main.py
@@ -7,7 +7,8 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+    print("This is a sample Python script.")
+    print("The script is used to compare two different scripts.")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
